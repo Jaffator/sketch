14fe73face8d9470a770d21532e20c0b2f2df84f
Jaffa <jardalufi@gmail.com>
2025-05-29 22:07:57 +0200

    Sketch is done
    added grid sizing, erasemode and clear button

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // default state after load or refresh page, grid of 16x16
 const grid = document.querySelector(".grid-container");
+const eraseswitch = document.querySelector("#erase-mode");
 const changeButton = document.querySelector("#changebtn");
 const gridSizeInput = document.querySelector("#number");
-changeButton.addEventListener("click", (event) => {
+gridSizeInput.addEventListener("input", () => {
+  let value = Number(gridSizeInput.value);
+  if (value > 100) gridSizeInput.value = 100;
+  if (value <= 0) gridSizeInput.value = 0;
   createGrid(gridSizeInput.value);
 });
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+  createGrid(gridSizeInput.value);
+});
+
+// changeButton.addEventListener("click", () => {
+//   createGrid(gridSizeInput.value);
+// });
 
 createGrid(16);
 
 function createGrid(size) {
+  grid.innerHTML = "";
   let numberofsquares = size * size;
   let squaresize = 20;
   let gridsize = size * squaresize;
@@ -17,14 +30,17 @@ function createGrid(size) {
     squaresize = 960 / size;
     gridsize = 960;
   }
-
   grid.style.width = gridsize + "px";
 
   for (let i = 0; i < numberofsquares; i++) {
     const square = document.createElement("div");
     grid.appendChild(square);
     square.addEventListener("mouseover", (event) => {
-      event.target.style.backgroundColor = "red";
+      let squarecolor = "";
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      if (eraseswitch.checked) squarecolor = "ffffff";
+      else squarecolor = randomColor;
+      event.target.style.backgroundColor = "#" + squarecolor;
     });
     square.style.width = squaresize + "px";
   }
